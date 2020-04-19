4201c959637f6839fafb8cb2b70058f0084d8df3
furimu <63914180+furimu@users.noreply.github.com>
2020-04-20 04:04:32 +0900

    Update date.py

--- config/date.py
+++ config/date.py
@@ -1,14 +1,15 @@
 import json
+import os
 
 DEFAULT_MESSAGE = {}
 
 def save(value, module):
     try:
+        os.mkdir("jsons")
         with open(f'config/jsons/{module}.json', 'w', encoding= 'utf-8') as f:
         return json.dump(value, f, ensure_ascii=False, indent=4)
 
     except FileExistsError:
-        os.mkdir("jsons")
         with open(f'config/jsons/{module}.json', 'w', encoding= 'utf-8') as f:
         return json.dump(value, f, ensure_ascii=False, indent=4)
 
