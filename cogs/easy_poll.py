68142b59d95a0331db23d4232ffab073b4935896
furimu <63914180+furimu@users.noreply.github.com>
2020-05-10 16:45:13 +0900

    Update easy_poll.py

--- cogs/easy_poll.py
+++ cogs/easy_poll.py
@@ -1,11 +1,11 @@
 from discord.ext import commands
-
+from utils import date
 import discord
 
 class Easy_Poll(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
-
+        self.load = date.load("poll_channel")
    @commands.command(aliases=['yn'])
     async def yneasy_poll(self, ctx, *, question=None):
         if question is None:
@@ -18,8 +18,13 @@ class Easy_Poll(commands.Cog):
         
 
     @commands.command(aliases=['pc'])
-    async def poll_channel(self, ctx, channel: discord.TextChannel=None):
-        if channel is None:
-            channel = ctx.channel
+    async def poll_channel(self, ctx, channel: discord.TextChannel, opt= None)
+        if opt is None:
+            opt= "global"
 
-        
+        if self.load[str(ctx.guild.id)].get("opt") is None:
+            self.load[str(ctx.guild.id)][opt]= None
+
+           self.load[str(ctx.guild.id)][opt]=str(channel.id)
+
+        self.save(self.load, "poll_channel")
