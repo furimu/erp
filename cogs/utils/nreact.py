4bccf2276033cf3bd6b302c6098fda687f413032
furimu <63914180+furimu@users.noreply.github.com>
2020-05-10 16:25:46 +0900

    Create nreact.py

---
+++ cogs/utils/nreact.py
@@ -0,0 +1,29 @@
+import discord
+
+def number_react(mes):
+    if k == '1':
+     await mes.add_reaction('\u0031\u20e3')
+ 
+                elif k == '2':
+                    await react_mes.add_reaction('\u0032\u20e3')
+ 
+                elif k == '3':
+                    await react_mes.add_reaction('\u0033\u20e3')
+ 
+                elif k == '4':
+                    await react_mes.add_reaction('\u0034\u20e3')
+ 
+                elif k == '5':
+                    await react_mes.add_reaction('\u0035\u20e3')
+ 
+                elif k == '6':
+                    await react_mes.add_reaction('\u0036\u20e3')
+ 
+                elif k == '7':
+                    await react_mes.add_reaction('\u0037\u20e3')
+ 
+                elif k == '8':
+                    await react_mes.add_reaction('\u0038\u20e3')
+ 
+                elif k == '9':
+                    await react_mes.add_reaction('\u0039\u20e3')
