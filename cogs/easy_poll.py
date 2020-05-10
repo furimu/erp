ed2c8d0142ef034e18a5a6fcf1a4d882cfe21e0e
furimu <63914180+furimu@users.noreply.github.com>
2020-05-10 16:35:35 +0900

    Update easy_poll.py

--- cogs/easy_poll.py
+++ cogs/easy_poll.py
@@ -6,7 +6,15 @@ class Easy_Poll(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
 
-    @commands.command(aliases=['ep']
-    async def easy_poll(self, ctx, *, question=None):
+   @commands.command(aliases=['yn'])
+    async def yneasy_poll(self, ctx, *, question=None):
         if question is None:
             return await ctx.send("質問が渡されていません")
+
+        e = discord.Embed(
+            title= question,
+            description= "**1**:はい, **2**:いいえ")
+
+        
+
+    @commands.command(aliases=['pc'])
