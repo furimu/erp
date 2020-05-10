726950211a3daa8814ff5832a60ba35a7470a8c4
furimu <63914180+furimu@users.noreply.github.com>
2020-05-10 18:22:50 +0900

    Update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -34,8 +34,8 @@ class Admin(commands.Cog, command_attrs=dict(hidden=True)):
     @load.error
     async def load_error(self, ctx, error):
         msg=traceback.format_exc()
-        for i in range(0, len(msg), 1094):
-            await ctx.channel.send(f'```py\n{msg[i:i+1094]}\n```')
+        for i in range(0, len(msg), 1092):
+            await ctx.channel.send(f'```py\n{msg[i:i+1092]}\n```')
 
     @commands.command()
     async def cac(self, ctx, category: discord.CategoryChannel, name: str):
