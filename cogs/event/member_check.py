35ab1e75f7230fa4649decb4cfe7651650479749
furimu <63914180+furimu@users.noreply.github.com>
2020-05-13 05:07:48 +0900

    Update member_check.py

--- cogs/event/member_check.py
+++ cogs/event/member_check.py
@@ -2,7 +2,7 @@ from discord.ext import commands, tasks
 from cogs.utils import keys
 import discord
 
-ID= keys.get_id()
+GUILDID, CHANNELID = keys.guild_id(), keys.channel_id()
 
 
 class Check_member(commands.Cog):
@@ -18,22 +18,33 @@ class Check_member(commands.Cog):
 
     @tasks.loop(hours = 6)
     async def check_member(self):
-        channel = self.bot.get_channel(699031258201718846)
+        channel = self.bot.get_channel(CHANNELID.profile)
         e = discord.Embed(
             title = '定期チェック',
             description = '定期でプロフィール未記入者に通知します'
         )
         await channel.send(embed = e)
-        guild = self.bot.get_guild(ID.mainguild)
+        guild = self.bot.get_guild(GUILDID.mainguild)
         role =
 discord.utils.get(guild.roles, name = 'not profile')
         for member in guild.members:
+            self.pro_count[str(member.id)]=0
             if any(r.name == role.name for r in member.roles):
                 await channel.send(f'{member.mention}さんプロフィールをお書きください。')
 
-        entrance= self.bot.get_channel(ID.entrance)
-        async for message in entrance.history(limit=None):
-            if self.pro_count.get(
+        async for message in channel.history(limit=None):
+            if len(message.mentions) ==0:
+                return
+
+            for user in message.mentions:
+            self.pro_count[str(user.id)] += 1
+
+                if self.pro_count[str(user.id)] <= 3:
+                    await user.klck()
+
+                    
+
+        
 
 
     @check_member.before_loop
