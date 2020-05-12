d2e6f229696f715c4f83b4bee622f54199a78cce
furimu <63914180+furimu@users.noreply.github.com>
2020-05-13 02:15:03 +0900

    Update err.py

--- cogs/event/err.py
+++ cogs/event/err.py
@@ -3,8 +3,9 @@ from discord.ext import commands
 class Error(commands.Cog):
     def __init__(self, bot):
         self.bot=bot
-
-        async def on_command_error(self, ctx, error):
+    
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
         error = getattr(error, 'original', error)
  
         input_error = (commands.CommandNotFound, commands.UserInputError
