f87c31f7fe691398082932d0a27ab562a9c7fd63
furimu <63914180+furimu@users.noreply.github.com>
2020-05-13 04:21:00 +0900

    Update keys.py

--- cogs/utils/keys.py
+++ cogs/utils/keys.py
@@ -3,5 +3,6 @@ from attrdict import AttrDict
 def get_id():
     return AttrDict({
         'update_news_channel': 693861839322218567,
-        'entrance': 696059891256656003
+        'entrance': 696059891256656003,
+        'mainguild': 695801973127118899
 })
