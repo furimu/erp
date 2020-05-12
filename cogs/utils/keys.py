c7b0cb52a5006608cbe0333debb49439119906f0
furimu <63914180+furimu@users.noreply.github.com>
2020-05-13 04:32:28 +0900

    Update keys.py

--- cogs/utils/keys.py
+++ cogs/utils/keys.py
@@ -1,8 +1,14 @@
 from attrdict import AttrDict
 
-def get_id():
+def guild_id():
     return AttrDict({
-        'update_news_channel': 693861839322218567,
-        'entrance': 696059891256656003,
         'mainguild': 695801973127118899
 })
+
+def channel_id():
+    return AttrDict({
+        'profile': 699031258201718846,
+        'update_news_channel': 693861839322218567,
+        'entrance': 696059891256656003
+    })
+
